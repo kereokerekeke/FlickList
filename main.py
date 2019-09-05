6f60b0a9908f7918c84a1d606e79d9039fdce3fa
Roman Tranquil <kereokerekeke@Landshark-3.local>
2019-09-05 19:18:16 -0400

    Completed Studio FlickList 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -8,11 +9,16 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
+    movie_tmw = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
     content += "<li>" + movie + "</li>"
+    content += "</ul>"
+    content += "<h1>Tomorrows's movie of the Day</h1>"
+    content += "<ul>"
+    content += "<li>" + movie_tmw + "</li>"
     content += "</ul>"
 
     # TODO: pick another random movie, and display it under
@@ -23,7 +29,12 @@ def index():
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    movies = ['Pulp Ficttion', 
+              'The Matrix',
+              'Groundhog day',
+              'Lion King',
+              'Ace Ventura']
+    return random.choice(movies)
 
 
 app.run()
