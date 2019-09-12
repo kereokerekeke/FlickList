8981e244491a1dafe4536d7142cffc1e46d98b6d
Roman Tranquil <kereokerekeke@Landshark-3.local>
2019-09-12 18:08:47 -0400

    Studio 2 completed

--- main.py
+++ main.py
@@ -38,16 +38,35 @@ add_form = """
 
 # a form for crossing off watched movies
 crossoff_form = """
-
+<form action="/crossoff" method="post">
+    <label for="crosoff-movie">
+        I want to cross off
+        <select id="crosoff-movie" name="crossed-off-movie">
+            {0}
+        </select>
+        from my Watchlist.
+    </label>
+    <input type="submit" value="Cross/Uncross">
+</form>
 """
+
+movies = []
 
 # TODO:
 # Finish filling in the function below so that the user will see a message like:
 # "Star Wars has been crossed off your watchlist".
 # And create a route above the function definition to receive and handle the request from 
 # your crossoff_form.
+@app.route("/crossoff", methods=['POST'])
 def crossoff_movie():
-    crossed_off_movie = request.form['crossed-off-movie']    
+    crossed_off_movie = request.form['crossed-off-movie']
+    
+    if '<strike>' in movies[movies.index(crossed_off_movie)]:
+        movies[movies.index(crossed_off_movie)] = crossed_off_movie.replace('strike','p')
+    else:
+        movies[movies.index(crossed_off_movie)] = '<strike>' + crossed_off_movie + '</strike>'
+
+    return index()
 
 # TODO:
 # modify the crossoff_form above to use a dropdown (<select>) instead of
@@ -56,21 +75,25 @@ def crossoff_movie():
 @app.route("/add", methods=['POST'])
 def add_movie():
     new_movie = request.form['new-movie']
-
+    movies.append(new_movie)
     # build response content
     new_movie_element = "<strong>" + new_movie + "</strong>"
-    sentence = new_movie_element + " has been added to your Watchlist!"
-    content = page_header + "<p>" + sentence + "</p>" + page_footer
 
-    return content
+    return index()
 
 
 @app.route("/")
 def index():
     edit_header = "<h2>Edit My Watchlist</h2>"
 
+    options = []
+    movies_list = []
+
+    for movie in movies:
+        options.append('<option value="' + movie + '">' + movie + "</option>")
+        movies_list.append('<li style="list-style: none;">' + movie + '</li>')
     # build the response string
-    content = page_header + edit_header + add_form + page_footer
+    content = page_header + edit_header + add_form + crossoff_form.format(''.join(options)) + '<h3>Movies list:</h3>' + '<ul>' +  ''.join(movies_list) + '</ul>' + page_footer
 
     return content
 
